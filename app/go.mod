56df778448b6f69f9ce535b5ffbfa4dd6408ab1a
reyesml <matthewlreyes+reyesml@gmail.com>
2022-12-18 22:25:28 -0800

    Added simple chi web server.

---
+++ app/go.mod
@@ -0,0 +1,5 @@
+module github.com/reyesml/RMT/app
+
+go 1.18
+
+require github.com/go-chi/chi/v5 v5.0.8 // indirect
---
+++ app/server/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"net/http"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello, world."))
+	})
+	http.ListenAndServe(":3000", r)
+}
