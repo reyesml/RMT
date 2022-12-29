e621db0343685023200a92ea155f72fc1e9696e1
reyesml <matthewlreyes+reyesml@gmail.com>
2022-12-29 01:58:01 -0800

    Adding authenticated route group

---
+++ web-client/src/routes/(authenticated)/+layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import '../../app.postcss';
+	import '../styles.css';
+	import Session from '$lib/components/session.svelte';
+</script>
+
+<Session>
+	<div class="app">
+		<main>
+			<slot />
+		</main>
+
+		<footer />
+	</div>
+</Session>
---
+++ web-client/src/routes/(authenticated)/home/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { getSessionStore } from '$lib/store/session';
+	let session = getSessionStore();
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="RMT Welcome" />
+</svelte:head>
+
+<section class="text-purple-800">
+	<h1>
+		Welcome to RMT, {$session.user.username}
+	</h1>
+
+	<div class="flex flex-col items-center">
+		<h2>
+			You can do anything. The only limit is <span class="italic underline">yourself</span>.
+		</h2>
+	</div>
+</section>
