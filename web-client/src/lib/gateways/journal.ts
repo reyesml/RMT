739b0e067c81dbaa2853b9ddb864e661fc8ca363
reyesml <matthewlreyes+reyesml@gmail.com>
2022-12-29 21:02:33 -0800

    removing console logs

--- web-client/src/lib/gateways/journal.ts
+++ web-client/src/lib/gateways/journal.ts
@@ -1,7 +1,6 @@
 const host = 'http://localhost:3000';
 
 export function create(auth: string, mood: string, title: string, body: string) {
-  console.log('calling create journal...')
 	return fetch(`${host}/journal`, {
 		method: 'POST',
 		cache: 'no-cache',
