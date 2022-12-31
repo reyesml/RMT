ccad0b88fcc2525fcdbc8f6951e4250cfcda652d
reyesml <matthewlreyes+reyesml@gmail.com>
2022-12-30 21:36:09 -0800

    Adding "+" button for new journals

--- web-client/src/routes/(authenticated)/journal/+page.svelte
+++ web-client/src/routes/(authenticated)/journal/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-	import JournalList from "./journal-list.svelte";
-
+	import PlusIcon from '$lib/components/icons/plus-icon.svelte';
+	import JournalList from './journal-list.svelte';
 </script>
 
 <svelte:head>
@@ -8,11 +8,16 @@
 	<meta name="description" content="RMT Journal" />
 </svelte:head>
 
-<section>
+<section class="relative">
 	<div class="flex min-h-full items-center justify-center py-12 px-4">
 		<div class="w-full max-w-xl space-y-8">
 			<h1>Journal</h1>
 			<JournalList />
 		</div>
 	</div>
+	<a href="/journal/new" class="fixed bottom-20 left-64 text-white">
+		<div class="bg-purple-700 rounded-full w-14 h-14 flex items-center justify-center">
+			<PlusIcon class="h-8 w-8" />
+		</div>
+	</a>
 </section>
