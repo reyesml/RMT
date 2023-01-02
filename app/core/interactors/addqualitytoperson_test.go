fab23a0ad7dbf7b6a7e90999130c59edb1dc50a2
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-01 21:17:28 -0800

    Created addQualityToPerson interactor and test.

---
+++ app/core/interactors/addqualitytoperson_test.go
@@ -0,0 +1,107 @@
+package interactors
+
+import (
+	"context"
+	"github.com/google/uuid"
+	"github.com/reyesml/RMT/app/core/database"
+	"github.com/reyesml/RMT/app/core/models"
+	"github.com/reyesml/RMT/app/core/repos"
+	"github.com/reyesml/RMT/app/core/utils"
+	"github.com/stretchr/testify/require"
+	"os"
+	"strings"
+	"testing"
+)
+
+func TestAddQualityToPerson_Execute(t *testing.T) {
+	testDBId := "TestAddQualityToPerson.db"
+	db, err := database.Connect(testDBId)
+	defer os.Remove(testDBId)
+
+	require.NoError(t, err)
+
+	err = utils.MigrateAllModels(db)
+	require.NoError(t, err)
+
+	user, err := models.NewUser("test-user", "plain_password")
+	require.NoError(t, err)
+	userRepo := repos.NewUserRepo(db)
+	require.NoError(t, userRepo.Create(user))
+
+	personRepo := repos.NewPersonRepo(db)
+	people := []models.Person{
+		{
+			Segmented: database.Segmented{SegmentUUID: user.SegmentUUID},
+			FirstName: "sam",
+			LastName:  "doe",
+			UserId:    user.ID,
+		},
+		{
+			Segmented: database.Segmented{SegmentUUID: user.SegmentUUID},
+			FirstName: "chris",
+			LastName:  "cross",
+			UserId:    user.ID,
+		},
+		{
+			Segmented: database.Segmented{SegmentUUID: user.SegmentUUID},
+			FirstName: "taylor",
+			LastName:  "smith",
+			UserId:    user.ID,
+		},
+	}
+	require.NoError(t, personRepo.CreateMany(&people))
+
+	qr := repos.NewQualityRepo(db)
+	pqr := repos.NewPersonQualityRepo(db)
+	aqtp := NewAddQualityToPerson(personRepo, qr, pqr)
+	ctx := utils.SetCurrentUser(context.Background(), *user, uuid.Nil)
+	cpqr := AddQualityToPersonRequest{
+		PersonUUID:  people[0].UUID,
+		QualityName: "Loves Writing Tests",
+	}
+	pq, err := aqtp.Execute(ctx, cpqr)
+	require.NoError(t, err)
+	require.NotEqual(t, uuid.Nil, pq.UUID)
+	require.Equal(t, user.SegmentUUID, pq.SegmentUUID)
+	require.Equal(t, user.ID, pq.UserId)
+	require.Equal(t, people[0].ID, pq.PersonId)
+
+	q, err := qr.GetByID(pq.QualityId, user.SegmentUUID)
+	require.NoError(t, err)
+	require.Equal(t, user.SegmentUUID, q.SegmentUUID)
+	require.NotEqual(t, uuid.Nil, q.UUID)
+	require.Equal(t, cpqr.QualityName, q.Name)
+	require.Equal(t, strings.ToLower(cpqr.QualityName), q.NameLower)
+
+	cpqr2 := AddQualityToPersonRequest{
+		PersonUUID:  people[1].UUID,
+		QualityName: "LOVES WRITING TESTS",
+	}
+	pq2, err := aqtp.Execute(ctx, cpqr2)
+	require.NoError(t, err)
+	require.NoError(t, err)
+	require.NotEqual(t, uuid.Nil, pq2.UUID)
+	require.Equal(t, user.SegmentUUID, pq2.SegmentUUID)
+	require.Equal(t, user.ID, pq2.UserId)
+	require.Equal(t, people[1].ID, pq2.PersonId)
+
+	// The quality had the same name as an existing quality,
+	// so the existing quality should have been used.
+	require.Equal(t, q.ID, pq2.QualityId)
+
+	cpqr3 := AddQualityToPersonRequest{
+		PersonUUID:  people[1].UUID,
+		QualityName: "Hates writing tests",
+	}
+	pq3, err := aqtp.Execute(ctx, cpqr3)
+	require.NoError(t, err)
+	require.NoError(t, err)
+	require.NotEqual(t, uuid.Nil, pq3.UUID)
+	require.Equal(t, user.SegmentUUID, pq3.SegmentUUID)
+	require.Equal(t, user.ID, pq3.UserId)
+	require.Equal(t, people[1].ID, pq3.PersonId)
+
+	// The quality doesn't match existing qualities,
+	// so a new one should have been created.
+	require.NotEqual(t, q.ID, pq3.QualityId)
+}
--- app/core/repos/qualities.go
+++ app/core/repos/qualities.go
@@ -4,13 +4,16 @@ import (
 	"github.com/google/uuid"
 	"github.com/reyesml/RMT/app/core/models"
 	"gorm.io/gorm"
+	"strings"
 )
 
 type QualityRepo interface {
 	Create(quality *models.Quality) error
 	CreateMany(qualities *[]models.Quality) error
 	GetByUUID(uuid uuid.UUID, segment uuid.UUID) (models.Quality, error)
+	GetByID(id uint, segment uuid.UUID) (models.Quality, error)
 	ListBySegment(segment uuid.UUID) ([]models.Quality, error)
+	FindByName(name string, userId uint, segment uuid.UUID) ([]models.Quality, error)
 }
 
 type qualityRepo struct {
@@ -22,11 +25,16 @@ func NewQualityRepo(db *gorm.DB) qualityRepo {
 }
 
 func (r qualityRepo) Create(quality *models.Quality) error {
+	quality.NameLower = strings.ToLower(quality.Name)
 	result := r.db.Create(quality)
 	return result.Error
 }
 
 func (r qualityRepo) CreateMany(qualities *[]models.Quality) error {
+	// TODO: test this to see if it actually works.
+	for _, q := range *qualities {
+		q.NameLower = strings.ToLower(q.Name)
+	}
 	result := r.db.Create(qualities)
 	return result.Error
 }
@@ -37,8 +45,20 @@ func (r qualityRepo) GetByUUID(uuid uuid.UUID, segment uuid.UUID) (models.Qualit
 	return q, result.Error
 }
 
+func (r qualityRepo) GetByID(id uint, segment uuid.UUID) (models.Quality, error) {
+	var q models.Quality
+	result := r.db.Where("id = ? and SEGMENT_UUID = ?", id, segment).First(&q)
+	return q, result.Error
+}
+
 func (r qualityRepo) ListBySegment(segment uuid.UUID) ([]models.Quality, error) {
 	var qs []models.Quality
 	result := r.db.Where("segment_uuid = ? ", segment).Find(&qs)
+	return qs, result.Error
+}
+
+func (r qualityRepo) FindByName(name string, userId uint, segment uuid.UUID) ([]models.Quality, error) {
+	var qs []models.Quality
+	result := r.db.Where("name_lower = ? and user_id = ? and segment_uuid = ?", strings.ToLower(name), userId, segment).Find(&qs)
 	return qs, result.Error
 }
