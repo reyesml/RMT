641b48f4829d10147e738300f952daab36737e0c
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-02 22:52:18 -0800

    Adding get and create to people gateway

--- web-client/src/lib/gateways/people.ts
+++ web-client/src/lib/gateways/people.ts
@@ -1,12 +1,35 @@
 const host = 'http://localhost:3000';
 
 export function list(auth: string) {
-	return fetch(`${host}/people/`, {
+	return fetch(`${host}/people`, {
 		method: 'GET',
 		cache: 'no-cache',
 		headers: {
 			'Content-Type': 'application/json',
 			Authorization: `Bearer ${auth}`
 		}
+	});
+}
+
+export function get(auth: string, uuid: string) {
+	return fetch(`${host}/people/${uuid}`, {
+		method: 'GET',
+		cache: 'no-cache',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${auth}`
+		}
+	});
+}
+
+export function create(auth: string, firstName: string, lastName: string) {
+  return fetch(`${host}/people`, {
+		method: 'POST',
+		cache: 'no-cache',
+		headers: {
+			'Content-Type': 'application/json',
+			Authorization: `Bearer ${auth}`
+		},
+		body: JSON.stringify({ firstName: firstName, lastName: lastName })
 	});
 }
