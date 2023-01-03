5ca97c74f0a0d15790e0a707de03548f6a546a17
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-03 00:18:05 -0800

    cleanup whitespace

--- web-client/src/routes/(authenticated)/journal/[uuid]/+page.svelte
+++ web-client/src/routes/(authenticated)/journal/[uuid]/+page.svelte
@@ -19,17 +19,17 @@
 				<h1>{data.error}</h1>
 			{:else if journal}
 				<h1 class="text-4xl break-words">{journal.title}</h1>
-                <div class="bg-black bg-opacity-20 rounded-3xl p-4 mt-6">
-                    <div class="whitespace-pre-wrap min-h-[300px]">
-                        {journal.body}
-                    </div>
-                    <div class="flex text-gray-400">
-                        <div>
-                            {journal.mood}
-                        </div>
-                        <div class="ml-auto">{new Date(journal.createdAt).toLocaleString()}</div>
-                    </div>
-                </div>
+				<div class="bg-black bg-opacity-20 rounded-3xl p-4 mt-6">
+					<div class="whitespace-pre-wrap min-h-[300px]">
+						{journal.body}
+					</div>
+					<div class="flex text-gray-400">
+						<div>
+							{journal.mood}
+						</div>
+						<div class="ml-auto">{new Date(journal.createdAt).toLocaleString()}</div>
+					</div>
+				</div>
 			{/if}
 		</div>
 	</div>
