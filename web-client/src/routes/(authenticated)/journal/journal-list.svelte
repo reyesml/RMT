1500d4f5251f8bc5e7d47d74bb194c030470763c
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-02 22:48:15 -0800

    cleaning up accidental alias

--- web-client/src/routes/(authenticated)/journal/journal-list.svelte
+++ web-client/src/routes/(authenticated)/journal/journal-list.svelte
@@ -5,9 +5,9 @@
 
 {#if journals}
 	<div class="flex flex-col divide-y">
-		{#each journals as { mood, title, createdAt, uuid: UUID }}
+		{#each journals as { mood, title, createdAt, uuid }}
 			<div class="py-2 px-2 w-full overflow-auto truncate">
-				<a href="/journal/{UUID}" class="text-2xl text-green-400 ">{title}</a>
+				<a href="/journal/{uuid}" class="text-2xl text-green-400 ">{title}</a>
 				<div class="flex ">
 					<div>{mood}</div>
 					<div class="ml-auto">{new Date(createdAt).toLocaleDateString()}</div>
