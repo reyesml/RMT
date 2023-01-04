91b9be6a08320cc917ff07ea7566c08367755b24
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-04 00:31:10 -0800

    Making add person ("+") button relative + sticky

--- web-client/src/routes/(authenticated)/people/+page.svelte
+++ web-client/src/routes/(authenticated)/people/+page.svelte
@@ -11,21 +11,25 @@
 	<meta name="description" content="RMT People" />
 </svelte:head>
 
-<section class="relative">
+<section>
 	<div class="flex min-h-full items-center justify-center py-12 px-4">
 		<div class="w-full max-w-xl">
 			<h1>People</h1>
-			{#if data.error}
-				Something went wrong.
-			{:else if data.people}
-				<PeopleList people={data.people} />
-			{/if}
+			<div class="relative">
+				<div class="absolute -left-20 h-full">
+					<a
+						href="/people/new"
+						class="sticky top-5 mt-5 text-white bg-purple-700 rounded-full w-14 h-14 flex items-center justify-center"
+					>
+						<PlusIcon class="h-8 w-8" />
+					</a>
+				</div>
+				{#if data.error}
+					Something went wrong.
+				{:else if data.people}
+					<PeopleList people={data.people} />
+				{/if}
+			</div>
 		</div>
 	</div>
-	<a
-		href="/people/new"
-		class="fixed bottom-20 left-64 text-white bg-purple-700 rounded-full w-14 h-14 flex items-center justify-center"
-	>
-		<PlusIcon class="h-8 w-8" />
-	</a>
 </section>
