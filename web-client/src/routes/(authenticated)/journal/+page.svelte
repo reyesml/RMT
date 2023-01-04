34b881bc367280d089a10140a5d04d25c9703e93
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-03 23:54:51 -0800

    Positioning "+" button relative to note list container, not fixed.

--- web-client/src/routes/(authenticated)/journal/+page.svelte
+++ web-client/src/routes/(authenticated)/journal/+page.svelte
@@ -11,21 +11,25 @@
 	<meta name="description" content="RMT Journal" />
 </svelte:head>
 
-<section class="relative">
+<section>
 	<div class="flex min-h-full items-center justify-center py-12 px-4">
 		<div class="w-full max-w-xl">
 			<h1>Journal</h1>
-			{#if data.error}
-				Something went wrong.
-			{:else if data.journals}
-				<JournalList journals={data.journals} />
-			{/if}
+			<div class="relative">
+				<div class="absolute -left-20 h-full">
+					<a
+						href="/journal/new"
+						class="sticky top-5 mt-5 text-white bg-purple-700 rounded-full w-14 h-14 flex items-center justify-center"
+					>
+						<PlusIcon class="h-8 w-8" />
+					</a>
+				</div>
+				{#if data.error}
+					Something went wrong.
+				{:else if data.journals}
+					<JournalList journals={data.journals} />
+				{/if}
+			</div>
 		</div>
 	</div>
-	<a
-		href="/journal/new"
-		class="fixed bottom-20 left-64 text-white bg-purple-700 rounded-full w-14 h-14 flex items-center justify-center"
-	>
-		<PlusIcon class="h-8 w-8" />
-	</a>
 </section>
