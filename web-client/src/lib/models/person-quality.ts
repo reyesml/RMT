821ee88d225c8befba821e555acf6a4b6e21d200
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-06 23:18:29 -0800

    Adding note count to quality list

--- web-client/src/lib/models/person-quality.ts
+++ web-client/src/lib/models/person-quality.ts
@@ -1,6 +1,9 @@
+import type {Note} from "$lib/models/note";
+
 export interface PersonQuality {
   uuid: string
   name: string
   type: string
   createdAt: string
+  notes?: Note[]
 }
--- web-client/src/routes/(authenticated)/people/[uuid]/quality-list.svelte
+++ web-client/src/routes/(authenticated)/people/[uuid]/quality-list.svelte
@@ -5,10 +5,15 @@
 
 <div class="flex flex-wrap gap-y-1 items-center mt-1 w-full">
 	<slot />
-	{#each qualities as { name, uuid }}
+	{#each qualities as { uuid, name, notes }}
 		<a
 			href="/person-quality/{uuid}"
-			class="text-md text-center text-white bg-green-600 py-1 px-2 mr-2 rounded-full">{name}</a
+			class="text-md text-center text-white bg-green-600 py-1 px-2 mr-2 rounded-full"
 		>
+			{name}
+			{#if notes?.length ?? 0 > 0}
+				&#x2022; {notes?.length}
+			{/if}
+		</a>
 	{/each}
 </div>
