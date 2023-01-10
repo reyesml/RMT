55e45be332a0d3cb099cd124202fd265c3fc42f2
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-09 21:35:27 -0800

    Adding search to home layout.

--- web-client/src/routes/(authenticated)/+layout@.svelte
+++ web-client/src/routes/(authenticated)/+layout@.svelte
@@ -5,21 +5,24 @@
 
 <Session>
 	<div class="mt-4 pb-10">
-		<main>
+		<main class="h-full w-full">
 			<slot />
 		</main>
 
-		<footer class="fixed inset-x-0 bottom-0 p-4 bg-gradient-to-t from-slate-900 to-transparent">
+		<footer class="fixed inset-x-0 bottom-0 pb-6 bg-gradient-to-t from-slate-900 to-transparent">
 			<div class="flex flex-col items-center">
 				<div class="flex flex-row items-center">
-					<a href="/journal" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
-						>Journal</a
+					<a href="/home" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
+						>Home</a
 					>
 					<a href="/people" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
 						>People</a
 					>
-					<a href="/settings" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
-						>Settings</a
+					<a href="/journal" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
+						>Journal</a
+					>
+					<a href="/initiatives" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
+						>Initiatives</a
 					>
 				</div>
 			</div>
--- web-client/src/routes/(authenticated)/home/+page.svelte
+++ web-client/src/routes/(authenticated)/home/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import MagIcon from '$lib/components/icons/mag-icon.svelte';
 	import { getSessionStore } from '$lib/store/session';
 	let session = getSessionStore();
 </script>
@@ -8,14 +9,19 @@
 	<meta name="description" content="RMT Welcome" />
 </svelte:head>
 
-<section class="text-purple-700">
+<section class="mt-40">
 	<h1>
-		Welcome to RMT, {$session.user.username}
+		Welcome, {$session.user.username} 👋
 	</h1>
 
-	<div class="flex flex-col items-center">
-		<h2 class="text-lg font-bold">
-			You can do anything. The only limit is <span class="italic underline">yourself</span>.
-		</h2>
+	<div class="w-full h-full flex flex-col justify-center items-center">
+		<div class="text-lg">The impossible is unknown.</div>
+		<div class="rounded-full w-1/2 h-16 bg-white mt-12 flex items-center py-3 px-3 border-4 border-transparent focus-within:border-indigo-500 focus-within:outline-none focus-within:ring-sky-500">
+			<MagIcon class="w-8 h-8 text-gray-600" />
+			<form class="flex-1 h-full flex items-center">
+				<label for="search" class="sr-only">Search</label>
+				<input type="text" name="search" id="search" class="w-full h-full bg-inherit text-black text-2xl ml-4 focus:outline-none" placeholder="search..."/>
+			</form>
+		</div>
 	</div>
 </section>
