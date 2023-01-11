ae4b7e051cada86bbc79ff9cdf2ff7722b9fee25
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-11 00:29:37 -0800

    cleaned up the footer a bit

--- web-client/src/routes/(authenticated)/+layout@.svelte
+++ web-client/src/routes/(authenticated)/+layout@.svelte
@@ -10,22 +10,23 @@
 			<slot />
 		</main>
 
-		<footer class="fixed inset-x-0 bottom-0 pb-6 bg-gradient-to-t from-slate-900 to-transparent">
-			<div class="flex flex-col items-center">
-				<div class="flex flex-row items-center">
-					<a href="/home" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
-						>Home</a
-					>
-					<a href="/people" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
-						>People</a
-					>
-					<a href="/journal" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
-						>Journal</a
-					>
-					<a href="/initiatives" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
-						>Initiatives</a
-					>
-				</div>
+		<footer
+			class="fixed flex flex-col items-center inset-x-0 bottom-0 justify-center h-20 bg-gradient-to-t from-slate-900 to-transparent"
+		>
+			<div class="flex flex-row items-center">
+				<a href="/home" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
+					>Home</a
+				>
+				<a href="/people" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
+					>People</a
+				>
+				<a href="/journal" class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center"
+					>Journal</a
+				>
+				<a
+					href="/initiatives"
+					class="bg-purple-700 rounded-md text-white w-32 py-1 mx-1 text-center">Initiatives</a
+				>
 			</div>
 		</footer>
 	</div>
