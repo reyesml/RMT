a37a942fbbf51f8e32f7a69863e1e622e84c20a6
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-11 00:54:30 -0800

    Adding redirect for /

--- web-client/src/routes/(public)/+page.svelte
+++ web-client/src/routes/(public)/+page.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
+	import { goto } from '$app/navigation';
 	import { getSessionStore } from '$lib/store/session';
 
 	let session = getSessionStore();
-</script>
 
-<h1>Home Page (Placeholder)</h1>
-<p>
-	You probably want {#if $session}
-		<a href="/home">Home</a>
-	{:else}
-		<a href="/login">Login</a>
-	{/if} instead.
-</p>
+	if(browser){
+		if ($session) {
+			goto('/home');
+		} else {
+			goto('/login');
+		}
+	}
+</script>
