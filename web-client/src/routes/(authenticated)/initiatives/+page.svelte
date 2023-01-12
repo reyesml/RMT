0b898b478277664bfc73407e61233e945161c5cc
reyesml <matthewlreyes+reyesml@gmail.com>
2023-01-11 23:24:07 -0800

    Placeholder page for initiatives

---
+++ web-client/src/routes/(authenticated)/initiatives/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>Initiatives</title>
+	<meta name="description" content="RMT Initiatives" />
+</svelte:head>
+
+<section class="mt-12">
+    <h1>Coming Soon</h1>
+    <div class="w-full mt-12 text-4xl flex justify-center">👷🚧</div>
+</section>
